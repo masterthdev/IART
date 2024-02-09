f2ea8d6febcc96d7acede36981d46ef6cc0f2c6c
masterthdev <64798939+masterthdev@users.noreply.github.com>
2024-02-09 12:56:22 +0300

    Update test_IART_reds_N6.py

--- test_IART_reds_N6.py
+++ test_IART_reds_N6.py
@@ -48,53 +48,53 @@ def main():
 
     avg_psnr_l = []
     avg_ssim_l = []
-    subfolder_l = lr_folder#sorted(glob.glob(osp.join(lr_folder, '*')))
-    subfolder_gt_l = gt_folder#sorted(glob.glob(osp.join(gt_folder, '*')))
+    subfolder = lr_folder#sorted(glob.glob(osp.join(lr_folder, '*')))
+    subfolder_gt = gt_folder#sorted(glob.glob(osp.join(gt_folder, '*')))
 
     # for each subfolder
     subfolder_names = []
-    for subfolder, subfolder_gt in zip(subfolder_l, subfolder_gt_l):
-        subfolder_name = osp.basename(subfolder)
-        subfolder_names.append(subfolder_name)
+    #for subfolder, subfolder_gt in zip(subfolder_l, subfolder_gt_l):
+    subfolder_name = osp.basename(subfolder)
+    subfolder_names.append(subfolder_name)
 
-        # read lq and gt images
-        imgs_lq, imgnames = read_img_seq(subfolder, return_imgname=True)
+    # read lq and gt images
+    imgs_lq, imgnames = read_img_seq(subfolder, return_imgname=True)
 
-        # calculate the iter numbers
-        length = len(imgs_lq)
+    # calculate the iter numbers
+    length = len(imgs_lq)
 
-        avg_psnr = 0
-        avg_ssim = 0
-        # inference
-        name_idx = 0
-        imgs_lq = imgs_lq.unsqueeze(0).to(device)
+    avg_psnr = 0
+    avg_ssim = 0
+    # inference
+    name_idx = 0
+    imgs_lq = imgs_lq.unsqueeze(0).to(device)
 
-        with torch.no_grad():
-            outputs = model(imgs_lq).squeeze(0)
+    with torch.no_grad():
+        outputs = model(imgs_lq).squeeze(0)
 
-        # convert to numpy image
-        for idx in range(outputs.shape[0]):
-            img_name = imgnames[name_idx] + '.png'
-            output = tensor2img(outputs[idx], rgb2bgr=True, min_max=(0, 1))
-            # read GT image
-            img_gt = cv2.imread(osp.join(subfolder_gt, img_name), cv2.IMREAD_UNCHANGED)
-            crt_psnr = psnr_ssim.calculate_psnr(
-                output, img_gt, crop_border=crop_border, test_y_channel=test_y_channel)
-            crt_ssim = psnr_ssim.calculate_ssim(
+    # convert to numpy image
+    for idx in range(outputs.shape[0]):
+        img_name = imgnames[name_idx] + '.png'
+        output = tensor2img(outputs[idx], rgb2bgr=True, min_max=(0, 1))
+        # read GT image
+        img_gt = cv2.imread(osp.join(subfolder_gt, img_name), cv2.IMREAD_UNCHANGED)
+        crt_psnr = psnr_ssim.calculate_psnr(
             output, img_gt, crop_border=crop_border, test_y_channel=test_y_channel)
-            # save
-            if save_imgs:
-                imwrite(output, osp.join(save_folder, subfolder_name, f'{img_name}'))
-            avg_psnr += crt_psnr
-            avg_ssim += crt_ssim
-            logger.info(f'{subfolder_name}--{img_name} - PSNR: {crt_psnr:.6f} dB. SSIM: {crt_ssim:.6f}')
-            name_idx += 1
+        crt_ssim = psnr_ssim.calculate_ssim(
+        output, img_gt, crop_border=crop_border, test_y_channel=test_y_channel)
+        # save
+        if save_imgs:
+            imwrite(output, osp.join(save_folder, subfolder_name, f'{img_name}'))
+        avg_psnr += crt_psnr
+        avg_ssim += crt_ssim
+        logger.info(f'{subfolder_name}--{img_name} - PSNR: {crt_psnr:.6f} dB. SSIM: {crt_ssim:.6f}')
+        name_idx += 1
 
-        avg_psnr /= name_idx
-        logger.info(f'name_idx:{name_idx}')
-        avg_ssim /= name_idx
-        avg_psnr_l.append(avg_psnr)
-        avg_ssim_l.append(avg_ssim)
+    avg_psnr /= name_idx
+    logger.info(f'name_idx:{name_idx}')
+    avg_ssim /= name_idx
+    avg_psnr_l.append(avg_psnr)
+    avg_ssim_l.append(avg_ssim)
 
     for folder_idx, subfolder_name in enumerate(subfolder_names):
         logger.info(f'Folder {subfolder_name} - Average PSNR: {avg_psnr_l[folder_idx]:.6f} dB. Average SSIM: {avg_ssim_l[folder_idx]:.6f}.')
